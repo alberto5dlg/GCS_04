17d167d03bbb8c489258af626b32928dd4ca9222
guixot <fguixotsoriano@gmail.com>
2017-03-14 16:30:42 +0100

    Crear Evento y hacer foto de perfil

--- EventosDeportivosApp/app/src/main/java/franciscoguixot/eventosdeportivosapp/CrearEvento.java
+++ EventosDeportivosApp/app/src/main/java/franciscoguixot/eventosdeportivosapp/CrearEvento.java
@@ -1,6 +1,5 @@
 package franciscoguixot.eventosdeportivosapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,19 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Eventos extends AppCompatActivity {
+public class CrearEvento extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eventos);
+        setContentView(R.layout.activity_crear_evento);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
-    public void goCrearEvento(View view) {
-        Intent i = new Intent(Eventos.this, CrearEvento.class);
-        startActivity(i);
-    }
 
 }
